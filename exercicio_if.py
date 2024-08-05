da51aede40c2511dbcb27ac203b5968ba3d2eac2
Patrick Perez <patrickperez1698@gmail.com>
2024-08-04 22:24:13 -0300

    tirando as linhas comentadas

--- exercicio_if.py
+++ exercicio_if.py
@@ -4,32 +4,32 @@
 # que todos os registros tenham valores positivos para `quantidade` e `preço`. 
 # Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
 # forem positivos ou "Dados inválidos" caso contrário.
-# produto : str = input('Digite seu produto ')
-# quantidade : int = int(input('Qual a quantidade '))
-# preco : float = float(input('Digite o preço do produto '))
+produto : str = input('Digite seu produto ')
+quantidade : int = int(input('Qual a quantidade '))
+preco : float = float(input('Digite o preço do produto '))
 
-# if produto and quantidade and preco < 0:
-#     print('Dados invalidos')
-# else:
-#     print(f'Dados validos, Seu produto {produto} tem essa quantidade {quantidade} e esse preço {preco}')
+if produto and quantidade and preco < 0:
+    print('Dados invalidos')
+else:
+    print(f'Dados validos, Seu produto {produto} tem essa quantidade {quantidade} e esse preço {preco}')
 ### Exercício 2: Classificação de Dados de Sensor
 # Imagine que você está trabalhando com dados de sensores IoT. 
 # Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
 # como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
 #%%
-# temperatura:float = float(input("Digite sua temperatura"))
+temperatura:float = float(input("Digite sua temperatura"))
 
-# try:
-#     if temperatura >= 36 and temperatura <= 37.5:
-#         print('Sua temperatura esta normal')
-#     elif temperatura >= 37.6 and temperatura <= 39.5:
-#         print('Você esta com febre')
-#     elif temperatura >= 39.6:
-#         print('Você esta com febre muito alta')
-#     else:
-#         print('Sua temperatura esta baixa')
-# except Exception as e:
-#     print(e)
+try:
+    if temperatura >= 36 and temperatura <= 37.5:
+        print('Sua temperatura esta normal')
+    elif temperatura >= 37.6 and temperatura <= 39.5:
+        print('Você esta com febre')
+    elif temperatura >= 39.6:
+        print('Você esta com febre muito alta')
+    else:
+        print('Sua temperatura esta baixa')
+except Exception as e:
+    print(e)
 
 
 ### Exercício 3: Filtragem de Logs por Severidade
@@ -38,17 +38,17 @@
 # como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
 # escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
 #%%
-# from datetime import datetime
-# data_atual = datetime.now()
+from datetime import datetime
+data_atual = datetime.now()
 
-# log = {
-#     'timeStamp': data_atual,
-#     'level' : 'ERROR',
-#     'message' : 'Falha na conexão'
-# }
+log = {
+    'timeStamp': data_atual,
+    'level' : 'ERROR',
+    'message' : 'Falha na conexão'
+}
 
-# if log['level'] == 'ERROR':
-#     print(log['message'])
+if log['level'] == 'ERROR':
+    print(log['message'])
 
 
 
@@ -58,15 +58,15 @@
 # fornecido um email válido. Escreva um programa que valide essas condições 
 # e imprima "Dados de usuário válidos" ou o erro específico encontrado.
 #%%
-# idade_valida : int = int(input('Qual é a sua idade '))
-# email_valido : str = input('Digite seu E-mail ')
+idade_valida : int = int(input('Qual é a sua idade '))
+email_valido : str = input('Digite seu E-mail ')
 
-# if idade_valida <= 18 and idade_valida >= 65:
-#     print('Idade invalida')
-# elif "@" not in email_valido or "." not in email_valido:
-#     print('Email inválido')
-# else:
-#     print('Dados válidos')
+if idade_valida <= 18 and idade_valida >= 65:
+    print('Idade invalida')
+elif "@" not in email_valido or "." not in email_valido:
+    print('Email inválido')
+else:
+    print('Dados válidos')
 
 
 
